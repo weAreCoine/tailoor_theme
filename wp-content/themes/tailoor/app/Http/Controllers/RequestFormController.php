1195967b3becf1efc3c8c4f8c284866f89a14e00
Coiné <barbi.luca@gmail.com>
2024-04-05 09:00:45 +0200

    Refactor RequestFormController for better readability
    This commit includes refactoring of the class RequestFormController in the tailoor theme. A new import Illuminate\Http\RedirectResponse was added and the function signature for submit() was updated to specify that a RedirectResponse is returned. Moreover, the code was cleaned to improve readability and potentially prevent future issues.

--- wp-content/themes/tailoor/app/Http/Controllers/RequestFormController.php
+++ wp-content/themes/tailoor/app/Http/Controllers/RequestFormController.php
@@ -4,28 +4,31 @@ namespace App\Http\Controllers;
 
 use App\Policies\RequestFormPolicy;
 use App\Services\RequestFormService;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 
-class RequestFormController extends Controller {
-    public function submit( Request $request ) {
-        if ( ! $request->has( 'requestForm_nonce' ) || wp_verify_nonce( $request->get( 'requestForm_nonce' ), 'requestForm_submit' ) !== 1 ) {
-            $errors                        = $_SESSION['requestFormErrors'] ?? [];
-            $errors[]                      = __( 'Something goes wrong. Please, try again in a few minutes.', 'sage' );
+class RequestFormController extends Controller
+{
+    public function submit(Request $request): RedirectResponse
+    {
+        if (!$request->has('requestForm_nonce') || wp_verify_nonce($request->get('requestForm_nonce'), 'requestForm_submit') !== 1) {
+            $errors = $_SESSION['requestFormErrors'] ?? [];
+            $errors[] = __('Something goes wrong. Please, try again in a few minutes.', 'sage');
             $_SESSION['requestFormErrors'] = $errors;
 
             return redirect()->back();
         }
-        [ $validated, $errors ] = RequestFormPolicy::validate( $request );
+        [$validated, $errors] = RequestFormPolicy::validate($request);
 
-        if ( ! empty( $errors ) ) {
+        if (!empty($errors)) {
             $_SESSION['requestFormErrors'] = $errors;
 
             return redirect()->back();
         }
 
-        RequestFormService::onSubmit( $validated );
+        RequestFormService::onSubmit($validated);
 
-        return redirect()->to( 'schedule-your-free-demo' );
+        return redirect()->to('schedule-your-free-demo');
     }
 }
