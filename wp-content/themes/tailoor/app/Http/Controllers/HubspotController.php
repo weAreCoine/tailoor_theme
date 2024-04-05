28fae6359c6ad64c0b8a5abb7de55a6eeff7374f
Coiné <barbi.luca@gmail.com>
2024-04-05 10:36:26 +0200

    Refactor HubspotController property mappings
    Simplified property mappings in HubspotController by combining the process of adding default attributes and mapping keys/values into a single function. This change streamlines the process of preparing properties for the HubSpot client, making the code cleaner and more efficient. It also facilitates easier future modifications to the mappings.

--- wp-content/themes/tailoor/app/Http/Controllers/HubspotController.php
+++ wp-content/themes/tailoor/app/Http/Controllers/HubspotController.php
@@ -26,9 +26,8 @@ class HubspotController
      */
     public function store(array $properties): bool
     {
-        $this->addDefaultAttributes($properties);
+        $properties = $this->mapProperties($properties);
 
-        $properties = $this->mapPropertiesValues($this->mapPropertiesKeys($properties));
         $company = $this->hubSpotClient->updateOrCreateCompany($this->filterCompaniesProperties($properties));
         $contact = $this->hubSpotClient->updateOrCreateContact($this->filterContactProperties($properties));
         $this->hubSpotClient->syncSubscriptions($properties);
@@ -42,41 +41,47 @@ class HubspotController
     }
 
     /**
-     * Add default attributes to the contact properties.
+     * Maps the keys and values of the provided properties array based on predetermined mappings and HS account setup.
      *
-     * @param array $properties The contact properties.
-     *                         - newsletter: A flag indicating whether the contact has subscribed to the newsletter.
-     *                         - lifecyclestage: The lifecycle stage of the contact.
-     *                         - hs_lead_status: The lead status of the contact.
-     *                         - hs_legal_basis: The legal basis of the contact.
+     * @param array $properties The properties array to be mapped.
+     * @return array The properties array with both keys and values mapped.
      */
-    protected function addDefaultAttributes(array &$properties): void
+    protected function mapProperties(array $properties): array
     {
-        /**
-         * FIXME Per quale motivo i nuovi lead assumono subito lo status ATTEMPTED_TO_CONTACT? Non sarebbe più corretto metterli NEW?
-         */
-        $properties['lifecyclestage'] = 'marketingqualifiedlead';
-        $properties['hs_lead_status'] = 'NEW';
-//        $properties['hs_lead_status'] = 'ATTEMPTED_TO_CONTACT';
 
         /**
+         * Adding default values
+         *
+         * FIXME Per quale motivo i nuovi lead assumono subito lo status ATTEMPTED_TO_CONTACT? Non sarebbe più corretto metterli NEW?
+         *
          * Il form nativo dava sempre il consenso completo (anche a fronte del rifiuto NL)
          * TODO Era un comportamento voluto e corretto? Se sì, eliminare il caso false
+         *
+         * - newsletter: A flag indicating whether the contact has subscribed to the newsletter.
+         * - lifecyclestage: The lifecycle stage of the contact.
+         * - hs_lead_status: The lead status of the contact.
+         * - hs_legal_basis: The legal basis of the contact.
          */
-        $properties['hs_legal_basis'] = $properties['newsletter'] ? 'Freely given consent from contact' : 'Legitimate interest – prospect/lead';
-    }
+        $mappedProperties = [
+            'lifecyclestage' => 'marketingqualifiedlead',
+            'hs_lead_status' => 'NEW',
+//            'hs_lead_status' => 'ATTEMPTED_TO_CONTACT',
+            'hs_legal_basis' => $properties['newsletter'] ? 'Freely given consent from contact' : 'Legitimate interest – prospect/lead',
+        ];
 
-    /**
-     * Maps the values of the provided properties array based on HS account setup.
-     *
-     * @param array $properties The properties array to map values for.
-     *
-     * @return array Returns the properties array with mapped values.
-     */
-    public function mapPropertiesValues(array $properties): array
-    {
         foreach ($properties as $key => $value) {
-            $properties[$key] = match ($key) {
+            // Map keys
+            $mappedKey = match ($key) {
+                'first_name' => 'firstname',
+                'last_name' => 'lastname',
+                'website' => 'domain',
+                'platform' => 'e_commerce_platform',
+                'business' => 'business_type',
+                default => $key
+            };
+
+            // Map values
+            $mappedValue = match ($key) {
                 'e_commerce_platform', 'platform' => match ($value) {
                     'woocommerce' => 'Wordpress',
                     default => ucfirst($value)
@@ -88,30 +93,12 @@ class HubspotController
                 },
                 default => $value
             };
+
+            // Assign mapped key and value
+            $mappedProperties[$mappedKey] = $mappedValue;
         }
 
-        return $properties;
-    }
-
-    /**
-     * Maps the keys of the properties array based on predetermined mappings from HS CRM.
-     *
-     * @param array $properties The properties array to be mapped.
-     * @return array The properties array with mapped keys.
-     */
-    protected function mapPropertiesKeys(array $properties): array
-    {
-        return array_combine(
-            array_map(fn(string $property) => match ($property) {
-                'first_name' => 'firstname',
-                'last_name' => 'lastname',
-                'website' => 'domain',
-                'platform' => 'e_commerce_platform',
-                'business' => 'business_type',
-                default => $property
-            }, array_keys($properties))
-            , array_values($properties)
-        );
+        return $mappedProperties;
     }
 
     /**
