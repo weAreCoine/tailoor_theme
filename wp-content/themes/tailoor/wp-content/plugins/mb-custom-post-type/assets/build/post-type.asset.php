aba230c1a80b0933ee98fd7c756500e0982416aa
Coiné <barbi.luca@gmail.com>
2025-01-09 11:26:11 +0100

    Delete unused plugin files and deprecated assets.
    Removed various obsolete files including plugin translations, assets like FontAwesome files, and PHP classes (About.php, Ajax.php). These changes help streamline the codebase and remove unnecessary dependencies.

--- wp-content/themes/tailoor/wp-content/plugins/mb-custom-post-type/assets/build/post-type.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'a8186c63c3cf6d8671f7');
--- wp-content/themes/tailoor/wp-content/plugins/mb-custom-post-type/assets/build/taxonomy.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '349d300fdb84ab791136');
