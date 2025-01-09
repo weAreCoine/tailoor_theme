4dd3967abe10c0d37b665153f4ecb8c7a80ec432
Coiné <barbi.luca@gmail.com>
2025-01-09 11:07:50 +0100

    Add new custom post type and taxonomy registration classes
    Introduced `PostTypeRegister` and `TaxonomyRegister` classes to handle custom post type and taxonomy registrations for the plugin. Added FontAwesome webfont assets and translation files for Brazilian Portuguese and Vietnamese to enhance functionality and localization support.

---
+++ wp-content/themes/tailoor/wp-content/plugins/mb-custom-post-type/vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wpmetabox/mb-custom-post-type',
+        'pretty_version' => '2.7.10',
+        'version' => '2.7.10.0',
+        'reference' => 'b2feb619f1d368863dee04c8e4b29c866689d93b',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'meta-box/support' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '99607eb4d8c8c96a7c882fc5a7c69cf3534d95bd',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../meta-box/support',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+        'wpmetabox/mb-custom-post-type' => array(
+            'pretty_version' => '2.7.10',
+            'version' => '2.7.10.0',
+            'reference' => 'b2feb619f1d368863dee04c8e4b29c866689d93b',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
